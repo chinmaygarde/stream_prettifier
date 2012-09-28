cf9afa15b4caf5ccf2f27806adb6cdda16468cac
Chinmay Garde <chinmay@apportable.com>
2012-09-28 12:03:29 -0700

    Moved UTF8 check earlier

--- lib/stream_prettifier.rb
+++ lib/stream_prettifier.rb
@@ -1,9 +1,7 @@
 require 'etc'
 require 'colorize'
-require 'iconv'
 
 CUSTOMIZATIONS = []
-ICONV = Iconv.new('UTF-8//IGNORE', 'UTF-8')
 
 class Customization
     def initialize(regex, action)
@@ -26,12 +24,11 @@ end
 
 def load_user_preferences
     user_pref = File.join(Etc.getpwuid.dir, ".stream_prettifier")
-    load user_pref if File.exists?(user_pref)    
+    load user_pref if File.exists?(user_pref)
 end
 
 def apply_color(line)
     CUSTOMIZATIONS.each do |c|
-        line = ICONV.iconv(line + ' ')[0..-2]
         line = c.action.call(line) if line.match(c.regex)
     end
     line
